cce0e6537cc5ca032a3976992623e1d92f9ac2b6
k.mitarai <kazmit299@gmail.com>
2023-10-04 07:29:35 +0900

    1回目(C++版)

---
+++ kumon01/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	for (int i = 1; i <= 100; ++i)
+	{
+		//判定処理をここに書いてください
+		 
+		//出力は以下の形式を使用してください
+		//std::cout << i << std::endl;		//数字を出力する
+		//std::cout << "hoge" << std::endl;	//文字列を出力する
+	}
+
+    return 0;
+}
